1afd064e544e6499f35797bfa6b71f7eb9ee3f06
Vadim Pasternak <vadimp@nvidia.com>
2023-07-31 18:16:40 +0000

    UBUNTU: SAUCE: mlxbf_gige: Add UPHY support for Nvidia Gigabit Ethernet driver
    Signed-off-by: Vadim Pasternak <vadimp@nvidia.com>

--- drivers/net/ethernet/mellanox/mlxbf_gige/mlxbf_gige.h
+++ drivers/net/ethernet/mellanox/mlxbf_gige/mlxbf_gige.h
@@ -100,6 +100,7 @@ struct mlxbf_gige {
 	struct platform_device *pdev;
 	void __iomem *mdio_io;
 	void __iomem *clk_io;
+	void __iomem *fuse_gw_io;
 	struct mii_bus *mdiobus;
 	spinlock_t lock;      /* for packet processing indices */
 	u16 rx_q_entries;
@@ -166,7 +167,8 @@ enum mlxbf_gige_res {
 	MLXBF_GIGE_RES_GPIO0,
 	MLXBF_GIGE_RES_LLU,
 	MLXBF_GIGE_RES_PLU,
-	MLXBF_GIGE_RES_CLK
+	MLXBF_GIGE_RES_CLK,
+	MLXBF_GIGE_RES_FUSE_GW
 };
 
 /* Version of register data returned by mlxbf_gige_get_regs() */
